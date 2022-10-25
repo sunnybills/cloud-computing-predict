cbadafb49135259976d47161af132e3a5d949984
sunnybills <111606228+sunnybills@users.noreply.github.com>
2022-10-25 11:49:49 +0100

    commit

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "{Insert your own AWS API Gateway Endpoint URL here}",
+                url: "https://8tdk3405t9.execute-api.eu-west-1.amazonaws.com/default/Test_Lamda_Predict_Function",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
